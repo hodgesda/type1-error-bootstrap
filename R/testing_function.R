f440e473b963de10e805c9da327246b328035f76
hodgesda <hodgesda@oregonstate.edu>
2018-11-17 00:19:11 +0000

    #2 function still needs name but otherwise works as intended.

--- R/testing_function.R
+++ R/testing_function.R
@@ -8,11 +8,16 @@ badfunctionname <- function(population, n, nboot, mu, alternative){
   #perform a t-test on the sample
   ttest <- t.test(samp, alternative=alternative, mu=mu)
   #bootstrap the sample using the clever iid thing with a matrix.
-  bstrap <- matrix(sample(samp, nboot*length(samp),replace=TRUE),ncol=nboot)
+  diff <- mu - mean(samp)
+  adj.samp <-samp + diff
+  bstrap <- matrix(sample(adj.samp, nboot*length(adj.samp),replace=TRUE),ncol=nboot)
   
-  #We want some p values now
-  c(ttest$p.value,
-  sum(colMeans(bstrap)>mu)/nboot)
+  #We want some p values now, look into switch function
+  if (alternative == "greater"){bootp <- sum(colMeans(bstrap)>= mean(samp))/nboot}
+  else if (alternative == "less"){bootp <- sum(colMeans(bstrap)<= mean(samp))/nboot}
+  else if (alternative == "two.sided"){bootp <- (sum(colMeans(bstrap)>= mu+abs(diff))+sum(colMeans(bstrap)<=mu-abs(diff)))/nboot}
+  else stop("Alternative must be 'greater','less', or'two.sided'")
+  c(ttest$p.value, bootp)
   
   #need to figure out output. I want the p-value from the t-test to come out and
   #a p-value from the bootstrap. I think I need some if-else clauses to deal with the
